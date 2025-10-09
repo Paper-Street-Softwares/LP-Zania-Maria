05a9f545c2eeac4019efb5f6f721c7a1799331c5
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-10-09 15:48:05 -0300

    fix: ajustes fontes

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -81,7 +81,7 @@ export default function Hero({
             <div className="flex justify-center desktop1:justify-start">
               <div className="flex flex-col items-center desktop1:flex-row text-secondary">
                 {content.texts.hero.obsHero.icon}
-                <p className="text-secondary  ml-[10px] text-center desktop1:text-start mt-[12px] desktop1:mt-0">
+                <p className="text-secondary  ml-[10px] text-center font-secondFont desktop1:text-start mt-[12px] desktop1:mt-0">
                   {t("hero.obsHero.text")}{" "}
                 </p>
               </div>
